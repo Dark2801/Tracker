c0a7547b46a118000ad178353dd07eabbf0b9258
Dark2801 <ripper2801@mail.ru>
2023-11-18 17:07:14 +0300

    Удалено лишнее

--- Tracker/TrackersTab/TrackersCollectionView/TrackerCollectionViewCell.swift
+++ Tracker/TrackersTab/TrackersCollectionView/TrackerCollectionViewCell.swift
@@ -131,13 +131,7 @@ final class TrackerCollectionViewCell: UICollectionViewCell {
         appendDayButton.setImage(image, for: .normal)
         appendDayButton.layer.opacity = opacity
     }
-    private func showAlert(_ message: String) {
-        let alert = UIAlertController(title: "Ошибка", message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default)
-        alert.addAction(okAction)
-    
-    }
-    
+
     private func checkDate() {
         let selectedDate = delegate?.getSelectedDate() ?? Date()
         appendDayButton.isEnabled = selectedDate <= currentDate ?? Date()
