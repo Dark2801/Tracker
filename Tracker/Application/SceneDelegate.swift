68e305f2cf3bdb8e099ae344c61d6d9bebbefcbf
Dark2801 <ripper2801@mail.ru>
2023-12-20 16:36:01 +0300

    добавил assets

---
+++ Tracker/Application/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  Tracker
+//
+//  Created by Андрей Мерзликин on 15.11.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+
+        window?.rootViewController = TabBarController()
+        window?.makeKeyAndVisible()
+    }
+}
+
