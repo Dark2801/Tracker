fe4d3927cf149412f98a46b2705d627c86335aa4
Dark2801 <ripper2801@mail.ru>
2023-12-20 17:00:47 +0300

    Создал newGroop -> Models: Tracker TrackerCategory TrackerRecord MockTrackers GeometryParams

---
+++ Tracker/Models/TrackerRecord.swift
@@ -0,0 +1,14 @@
+//
+//  TrackerRecord.swift
+//  Tracker
+//
+//  Created by Андрей Мерзликин on 20.12.2023.
+//
+
+import UIKit
+
+struct TrackerRecord: Hashable {
+    let id: UUID
+    let date: Date
+    var days: Int
+}
