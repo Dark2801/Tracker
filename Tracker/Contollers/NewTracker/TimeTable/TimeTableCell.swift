4a70df2ca7181565486ab107f26c271ce966c4b3
Dark2801 <ripper2801@mail.ru>
2023-12-21 18:59:39 +0300

    Реализация выбора даты

---
+++ Tracker/Contollers/NewTracker/TimeTable/TimeTableCell.swift
@@ -0,0 +1,67 @@
+//
+//  TimeTableCell.swift
+//  Tracker
+//
+//  Created by Андрей Мерзликин on 21.12.2023.
+//
+
+import UIKit
+
+protocol TimeTableCellDelegate: AnyObject {
+    func didToogleSwitch(for day: String, isOn: Bool)
+}
+
+final class TimeTableCell: UITableViewCell {
+    
+    weak var delegateCell: TimeTableCellDelegate?
+    
+    lazy var switchDay: UISwitch = {
+        let switchDay = UISwitch()
+        switchDay.onTintColor = .systemBlue
+        switchDay.addTarget(self, action: #selector(switchTapped), for: .valueChanged)
+        return switchDay
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Action
+    @objc
+    private func switchTapped(_ sender: UISwitch) {
+        if let daysOfWeek = textLabel?.text {
+            let shortDay = shortDays(for: daysOfWeek)
+            delegateCell?.didToogleSwitch(for: shortDay, isOn: sender.isOn)
+        }
+    }
+    
+    // MARK: Shorten the Days
+    private func shortDays(for day: String) -> String {
+        switch day {
+        case "Понедельник": return "Пн"
+        case "Вторник": return "Вт"
+        case "Среда": return "Ср"
+        case "Четверг": return "Чт"
+        case "Пятница": return "Пт"
+        case "Суббота": return "Сб"
+        case "Воскресенье": return "Вс"
+        default: return ""
+        }
+    }
+}
+
+private extension TimeTableCell {
+    func setupCell() {
+        contentView.addSubviews(switchDay)
+        
+        NSLayoutConstraint.activate([
+            switchDay.centerYAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.centerYAnchor),
+            switchDay.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+    }
+}
