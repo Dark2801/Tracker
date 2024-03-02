f63f06774da116a0d6f9270fcb869f3328cc2191
Dark2801 <ripper2801@mail.ru>
2024-03-02 15:13:09 +0300

    добавлен UIColorExtension

---
+++ Tracker/Helpers/Extension/UIColorExtension.swift
@@ -0,0 +1,43 @@
+//
+//  UIColor+Extension.swift
+//  Tracker
+//
+//  Created by Андрей Мерзликин on 20.12.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBackgroundDay: UIColor { UIColor(named: "YP Background Day") ?? UIColor.lightGray }
+    
+    static var ypBlackDay: UIColor { UIColor(named: "YP Black Day") ?? UIColor.black }
+    static var ypWhiteDay: UIColor { UIColor(named: "YP White Day") ?? UIColor.white }
+    
+    static var ypBlue: UIColor { UIColor(named: "YP Blue") ?? UIColor.blue }
+    static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    
+    static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+    static var ypLightGray: UIColor { UIColor(named: "YP Light Gray") ?? UIColor.lightGray }
+    static var ypDatePickerBackground: UIColor { UIColor(named: "YP DatePicker Background") ?? UIColor.lightGray }
+    static var ypEmojiBackground: UIColor { UIColor(named: "YP Emoji Background") ?? UIColor.white }
+    static var ypBackground: UIColor { UIColor(named: "YP Background (Alpha 12)") ?? UIColor.lightGray }
+    
+    static var ypColorSelection1: UIColor { UIColor(named: "Color selection 1") ?? UIColor.red }
+    static var ypColorSelection2: UIColor { UIColor(named: "Color selection 2") ?? UIColor.orange }
+    static var ypColorSelection3: UIColor { UIColor(named: "Color selection 3") ?? UIColor.blue }
+    static var ypColorSelection4: UIColor { UIColor(named: "Color selection 4") ?? UIColor.purple }
+    static var ypColorSelection5: UIColor { UIColor(named: "Color selection 5") ?? UIColor.green }
+    static var ypColorSelection6: UIColor { UIColor(named: "Color selection 6") ?? UIColor.systemPink }
+    static var ypColorSelection7: UIColor { UIColor(named: "Color selection 7") ?? UIColor.systemPink }
+    static var ypColorSelection8: UIColor { UIColor(named: "Color selection 8") ?? UIColor.blue }
+    static var ypColorSelection9: UIColor { UIColor(named: "Color selection 9") ?? UIColor.green }
+    static var ypColorSelection10: UIColor { UIColor(named: "Color selection 10") ?? UIColor.blue }
+    static var ypColorSelection11: UIColor { UIColor(named: "Color selection 11") ?? UIColor.red }
+    static var ypColorSelection12: UIColor { UIColor(named: "Color selection 12") ?? UIColor.systemPink }
+    static var ypColorSelection13: UIColor { UIColor(named: "Color selection 13") ?? UIColor.orange }
+    static var ypColorSelection14: UIColor { UIColor(named: "Color selection 14") ?? UIColor.blue }
+    static var ypColorSelection15: UIColor { UIColor(named: "Color selection 15") ?? UIColor.purple }
+    static var ypColorSelection16: UIColor { UIColor(named: "Color selection 16") ?? UIColor.purple }
+    static var ypColorSelection17: UIColor { UIColor(named: "Color selection 17") ?? UIColor.purple }
+    static var ypColorSelection18: UIColor { UIColor(named: "Color selection 18") ?? UIColor.green }
+}
