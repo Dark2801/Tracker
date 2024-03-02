596440a225d7c30a900ad3d43d0dcdead172b627
Dark2801 <ripper2801@mail.ru>
2024-03-02 19:15:06 +0300

    Добавлен TrackerCategory

---
+++ Tracker/Models/TrackerCategory.swift
@@ -0,0 +1,11 @@
+//
+//  TrackerCategory.swift
+//  Tracker
+//
+//  Created by Андрей Мерзликин on 02.03.2024.
+//
+
+struct TrackerCategory {
+    let title: String
+    let trackers: [Tracker]
+}
