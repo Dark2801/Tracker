c6ae78286f2d43bbd852100150550fa90cc12234
Dark2801 <ripper2801@mail.ru>
2024-03-02 19:17:59 +0300

    Добавлены дни недели

---
+++ Tracker/Models/TrackerSchedule.swift
@@ -0,0 +1,56 @@
+//
+//  TrackerSchedule.swift
+//  Tracker
+//
+//  Created by Андрей Мерзликин on 02.03.2024.
+//
+
+struct TrackerSchedule {
+    enum DaysOfTheWeek: Int, CaseIterable {
+        case monday = 2
+        case tuesday = 3
+        case wednesday = 4
+        case thursday = 5
+        case friday = 6
+        case saturday = 7
+        case sunday = 1
+        
+        var daysNames: String {
+            switch self {
+            case .monday:
+                return "Понедельник"
+            case .tuesday:
+                return "Вторник"
+            case .wednesday:
+                return "Среда"
+            case .thursday:
+                return "Четверг"
+            case .friday:
+                return "Пятница"
+            case .saturday:
+                return "Суббота"
+            case .sunday:
+                return "Воскресенье"
+            }
+        }
+        
+        var shortName: String {
+            switch self {
+            case .monday:
+                return "Пн"
+            case .tuesday:
+                return "Вт"
+            case .wednesday:
+                return "Ср"
+            case .thursday:
+                return "Чт"
+            case .friday:
+                return "Пт"
+            case .saturday:
+                return "Сб"
+            case .sunday:
+                return "Вс"
+            }
+        }
+    }
+}
