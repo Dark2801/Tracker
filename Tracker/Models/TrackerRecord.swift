cd977b6ee9e28c4a3a9b92957f50f0afb1ca4249
Dark2801 <ripper2801@mail.ru>
2024-03-02 19:16:23 +0300

    Добавлен TrackerRecord

---
+++ Tracker/Models/TrackerRecord.swift
@@ -0,0 +1,13 @@
+//
+//  TrackerRecord.swift
+//  Tracker
+//
+//  Created by Андрей Мерзликин on 02.03.2024.
+//
+
+import Foundation
+
+struct TrackerRecord {
+    let trackerId: UUID
+    let date: Date
+}
