8bff212ee9c4868af547b93288680f26ed723f2a
Dark2801 <ripper2801@mail.ru>
2024-03-03 13:43:08 +0300

    Создание Регулярных и Не Регулярных событий

---
+++ Tracker/AddingTracker/NewTracker/IrregularEvent/EventEmojiCell.swift
@@ -0,0 +1,48 @@
+//
+//  EventEmojiCell.swift
+//  Tracker
+//
+//  Created by Андрей Мерзликин on 03.03.2024.
+//
+
+import UIKit
+
+final class EventEmojiCell: UICollectionViewCell {
+    
+    static var reuseId = "Event emoji cell"
+    
+    lazy var emojiLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 32)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override var isSelected: Bool {
+        didSet {
+            contentView.backgroundColor = isSelected ? .ypLightGray : .ypWhiteDay
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.addSubview(emojiLabel)
+        
+        NSLayoutConstraint.activate([
+            emojiLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            emojiLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        contentView.addSubview(emojiLabel)
+        
+        NSLayoutConstraint.activate([
+            emojiLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            emojiLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+    }
+}
