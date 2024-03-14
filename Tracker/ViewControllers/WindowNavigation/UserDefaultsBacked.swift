b24854def62a19f14a1e0b551496a4e4ff8ea52a
Dark2801 <ripper2801@mail.ru>
2024-03-14 21:13:04 +0300

    Окно навигации

---
+++ Tracker/ViewControllers/WindowNavigation/UserDefaultsBacked.swift
@@ -0,0 +1,23 @@
+//
+//  UserDefaultsBacked.swift
+//  Tracker
+//
+//  Created by Андрей Мерзликин on 14.03.2024.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefaultsBacked<Value> {
+    let key: String
+    let storage: UserDefaults = .standard
+    
+    var wrappedValue: Value? {
+        get {
+            storage.value(forKey: key) as? Value
+        }
+        set {
+            storage.setValue(newValue, forKey: key)
+        }
+    }
+}
