8743e76a2d33be992c5743b59634358060b3f9bb
Dark2801 <ripper2801@mail.ru>
2024-03-14 20:33:32 +0300

    CellSelectionProtocol

---
+++ Tracker/Helpers/CellSelectionProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  CellSelectionProtocol.swift
+//  Tracker
+//
+//  Created by Андрей Мерзликин on 14.03.2024.
+//
+
+import Foundation
+
+protocol CellSelectionProtocol {
+    func select()
+    func deselect()
+}
