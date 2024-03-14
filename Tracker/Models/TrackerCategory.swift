16f48d547851f0cfd630c6ef58e43078ebdf62ee
Dark2801 <ripper2801@mail.ru>
2024-03-14 20:55:14 +0300

    Добавлены модели

---
+++ Tracker/Models/TrackerCategory.swift
@@ -0,0 +1,34 @@
+//
+//  TrackerCategory.swift
+//  Tracker
+//
+//  Created by Андрей Мерзликин on 14.03.2024.
+//
+
+import Foundation
+
+struct TrackerCategory: Equatable {
+    let name: String
+    let id: UUID
+    
+    init(name: String, id: UUID = UUID()) {
+        self.name = name
+        self.id = id
+    }
+    
+    var data: Data {
+        Data(name: name, id: id)
+    }
+}
+
+extension TrackerCategory {
+    struct Data {
+        var name: String
+        let id: UUID
+        
+        init(name: String = "", id: UUID? = nil) {
+            self.name = name
+            self.id = id ?? UUID()
+        }
+    }
+}
---
+++ Tracker/TrackerStore/TrackerStore.swift
@@ -0,0 +1,8 @@
+//
+//  TrackerStore.swift
+//  Tracker
+//
+//  Created by Андрей Мерзликин on 14.03.2024.
+//
+
+import Foundation
